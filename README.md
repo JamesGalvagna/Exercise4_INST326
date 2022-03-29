2b8b8fea3d561801288a05e1c3d7952199f49630
James Galvagna <jgalvagn@umd.edu>
2022-03-29 13:35:26 -0400

    first changes to exercise 4

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Exercise4_INST326
+this is exercise 4 for our INST 326 class
+
---
+++ ex4.py
@@ -0,0 +1,14 @@
+'''
+A simple function that says hello to the user
+Group Members:
+    - James Gavagna
+    - ENTER NAME
+
+'''
+
+
+
+def hello_user(name):
+    print(f"Hello {name}")
+    
+hello_user("James")
